208885d76ba0d4f1f7a9dd35847af4c1bb70a9c3
hoffm386 <erinrhof@uw.edu>
2019-09-19 19:45:05 -0700

    setup for 062419 first flask lecture

--- app.py
+++ app.py
@@ -1,17 +1,9 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path="")
+app = Flask(__name__, static_url_path="/static")
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/")
 def index():
-    """Return the main page.  If the user has submitted any input, it will be rendered as output"""
-    if request.method == "GET":
-        # if this is the initial request for the page, just render the index
-        return render_template("index.html")
-    else:
-        # if the user has submitted some data, retrieve it and send it as an argument
-        data = request.form
-        print(data)
-        return render_template("index.html", user_output=data["user_input"])
-
+    """Return the main page."""
+    return render_template("index.html")
 
